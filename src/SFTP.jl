1f4d874a2cd8c6afdc17a42d5d18edc4cd3dba51
Peter Bräuer <pb866.git@gmail.com>
2025-01-20 08:32:05 +0100

    Exclude function in codecov
    Signed-off-by: Peter Bräuer <pb866.git@gmail.com>

--- src/SFTP.jl
+++ src/SFTP.jl
@@ -2,7 +2,7 @@ module SFTP
 
 f(x::Int) = x^2
 f(x::Float64) = round(x)^2
-function verbosesign(x::Real)
+function verbosesign(x::Real) #GCOVR_EXCL_FUNCTION
     if x < 0
         return "negative"
     elseif x > 0
